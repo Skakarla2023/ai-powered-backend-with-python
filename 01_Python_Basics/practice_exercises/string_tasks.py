3d24adf29deabacb81350573106784ce13972326
Skakarla2023 <skakarla2023@gmail.com>
2025-10-24 10:39:37 +0530

    Update string_tasks.py

--- 01_Python_Basics/practice_exercises/string_tasks.py
+++ 01_Python_Basics/practice_exercises/string_tasks.py
@@ -1,1 +1,50 @@
+s = "hello world"
 
+print(len(s))
+
+for x in s:
+    print(x)
+
+
+print(s[:1])
+print(s[2:])
+print(s[:-1])
+
+print("is" not in s)
+
+if "is" not in s:
+    print(" is is absent in s")
+
+# convert into uppercase
+print(s.upper())
+
+# convert into lowercase
+print(s.lower())
+
+# to remove whitespaces use strip()
+print(s.strip())
+
+# the replace function replaces a string with another string
+print(s.replace("h","t"))
+
+
+a = "hello"
+b = "world"
+# string concatenation
+c = a+b
+print(c)
+
+c = a+" "+b
+print(c)
+
+# string formatting in python, 
+# usually we cannot print numbers and string together in python, for that we use fstrings
+cost = 59
+txt = f"The bag costs {cost} rupees"
+print(txt)
+
+txt2 = f"The price is {cost:.3f} rupees"
+print(txt2)
+
+# to insert a special character inside a string use \(backslashes)
+print("What's so \"special\" about VS")
