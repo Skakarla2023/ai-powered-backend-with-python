9b2f6c7f2982605e226941bd14df61c4cb9479e1
Skakarla2023 <skakarla2023@gmail.com>
2025-11-07 08:15:07 +0530

    Update list_tasks.py

--- 01_Python_Basics/practice_exercises/list_tasks.py
+++ 01_Python_Basics/practice_exercises/list_tasks.py
@@ -112,3 +112,12 @@ list2 = [1, 2, 3]
 list3 = list1 + list2
 print(list3)
 
+# Another way to join two lists is by appending all the items from list2 into list1, one by one:
+
+list1 = ["a", "b" , "c"]
+list2 = [1, 2, 3]
+
+for x in list2:
+  list1.append(x)
+
+print(list1)
