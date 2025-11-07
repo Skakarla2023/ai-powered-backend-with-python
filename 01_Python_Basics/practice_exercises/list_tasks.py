d0a1fe087ea0ff6ef79f2c48d9af30fae980e7bf
Skakarla2023 <skakarla2023@gmail.com>
2025-11-07 08:12:31 +0530

    Update list_tasks.py

--- 01_Python_Basics/practice_exercises/list_tasks.py
+++ 01_Python_Basics/practice_exercises/list_tasks.py
@@ -56,3 +56,58 @@ print(mylist)
 mylist.clear()
 print(mylist)
 
+# Loop through list items using for loop or while loop
+mylist=[1,2,3,4,5,6,7]
+for i in mylist:
+    print(i)
+for i in range(len(mylist)):
+    print(mylist[i])
+i=0
+while i<len(mylist):
+    print(mylist[i])
+    i=i+1
+
+# You can sort a list by using sort() function
+list1=[6,3,5,1,8,9,4]
+list1.sort()
+print(list1)
+
+list1.sort(reverse=True)
+print(list1)
+
+def myfunc(n):
+    return n-1
+    
+list2=[34,68,32,90,55,12,14,5]
+list2.sort(key=myfunc)
+print(list2)
+
+
+# By default the sort() method is case sensitive, resulting in all capital letters being sorted before lower case letters
+
+# You can use the reverse function to reverse a list
+list2.reverse()
+print(list2)
+
+# You can use the built-in List method copy() to copy a list.
+thislist = ["apple", "banana", "cherry"]
+mylist = thislist.copy()
+print(mylist)
+
+# Another way to make a copy is to use the built-in method list().
+thislist = ["apple", "banana", "cherry"]
+mylist = list(thislist)
+print(mylist)
+
+# You can also make a copy of a list by using the : (slice) operator.
+thislist = ["apple", "banana", "cherry"]
+mylist = thislist[:]
+print(mylist)
+
+
+# Join 2 liss using the + operator
+list1 = ["a", "b", "c"]
+list2 = [1, 2, 3]
+
+list3 = list1 + list2
+print(list3)
